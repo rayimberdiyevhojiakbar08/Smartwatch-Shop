5edb2c633af48470bd27cda2e97d4ae038b769f1
rayimberdiyevhojiakbar08 <rayimberdiyevhojiakbar08@gmail.com>
2023-06-09 11:52:47 +0500

    port change

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const app = new express();
 
 middleware(app);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, "0.0.0.0",  () => console.log(`Listening on port ${port}...`));
